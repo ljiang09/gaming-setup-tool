2fc5b9efa10288224a038880e535c54842d45586
ljiang09 <ljiang@olin.edu>
2023-09-25 00:53:25 -0400

    feat: placeholders for imgs and blurbs

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -1,5 +1,42 @@
+import { Grid, Typography } from "@mui/material";
+
 const AboutUs = () => {
-  return <div>Lily and Aditi</div>;
+  return (
+    <Grid container style={{ marginTop: 30 }}>
+      <Grid
+        item
+        xs={6}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4">Lily Jiang</Typography>
+        <div style={{ background: "#D3D3D3", height: 200, width: 200 }}>
+          Picture Here
+        </div>
+        <Typography variant="body1">Short Description Here</Typography>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4">Aditi Vinod</Typography>
+        <div style={{ background: "#D3D3D3", height: 200, width: 200 }}>
+          Picture Here
+        </div>
+        <Typography variant="body1">Short Description Here</Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default AboutUs;
