0e8f236e2d9df79c6c27842bb0e52f633a7508db
ljiang09 <ljiang@olin.edu>
2023-09-24 23:48:17 -0400

    feat: upload img file button

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,49 @@
+import { React, useEffect, useState } from "react";
+import { Button, Box } from "@mui/material";
+
 const Search = () => {
-  return <div>Search bar here</div>;
+  const [imgFile, setImgFile] = useState(null);
+  const [imgPreview, setImgPreview] = useState(null);
+
+  const handleFileChange = (event) => {
+    setImgFile(event.target.files[0]);
+  };
+
+  // store uploaded file as base64 to display
+  useEffect(() => {
+    if (imgFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImgPreview(reader.result);
+      };
+      reader.readAsDataURL(imgFile);
+    } else {
+      setImgPreview(null);
+    }
+  }, [imgFile]);
+
+  return (
+    <>
+      <Button variant="outlined" component="label">
+        {imgFile ? "Edit Image" : "Upload Image"}
+        <input
+          type="file"
+          accept="image/*"
+          hidden
+          onChange={handleFileChange}
+        />
+      </Button>
+      <Box width={200} style={{ margin: "auto", marginTop: "20px" }}>
+        {imgPreview && (
+          <img
+            src={imgPreview}
+            alt="preview"
+            style={{ width: "100%", height: "100%" }}
+          />
+        )}
+      </Box>
+    </>
+  );
 };
 
 export default Search;
