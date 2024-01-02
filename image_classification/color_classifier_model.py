880b27eb790acd0c57d4175ca716a6a736d27ac8
ljiang09 <ljiang@olin.edu>
2024-01-02 13:20:42 -0600

    skeleton for color classifier model

---
+++ image_classification/color_classifier_model.py
@@ -0,0 +1,24 @@
+def make_model():
+    '''
+    Creates and saves a classification algorithm based purely on color
+    '''
+    pass
+
+def get_nearest_neighbors(image):
+    '''
+    Grabs the image's nearest "color neighbors" from the model
+
+    Args:
+    image: a PIL image object
+
+    Returns:
+    An array of strings, each representing an id for a keyboard
+    or mouse image
+    '''
+    # load model
+    # put image through
+    # find nearby images
+    # get the image paths
+    # trim extensions and store the image IDs in an array to return
+    pass
+
