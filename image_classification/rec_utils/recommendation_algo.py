bb518c86df12b64cecbc93c83babf45f7ae07dd5
ljiang09 <ljiang@olin.edu>
2024-01-01 23:35:28 -0600

    start recommendation algorithm

---
+++ image_classification/rec_utils/recommendation_algo.py
@@ -0,0 +1,20 @@
+# csv with data? so we can use numpy to work with it
+# name, price, link, img url, colors
+# eventually add tags
+
+
+THRESHOLD = 50  # how close of a color is acceptable
+
+def rec_by_colors(colors):
+    '''
+    Gets a list of 3 keyboard and 3 mice based purely on color.
+
+    Args:
+    colors: an array of integer tuples representing rgb values like so:
+        (255, 255, 255)
+    
+    Returns:
+    An array of data for 3 mice and 3 keyboards. This data is of the
+    form of a subarray
+    '''
+    pass
